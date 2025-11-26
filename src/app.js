7daa8c38439f3b262be021bf14ec9ee141a7902e
luis-felipe-sierra-franco <luisfelipesierrafranco@gmail.com>
2025-11-26 08:08:34 -0500

    chore(commit 2): configure prisma and database schema

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import prisma from "./prisma.js"; // 👈 Importa Prisma
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API running");
+// Solo para probar que Prisma funciona
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany(); // 👈 Usa Prisma
+  res.json({
+    message: "API running with Prisma connected",
+    users,
+  });
 });
 
 const PORT = process.env.PORT || 3000;
